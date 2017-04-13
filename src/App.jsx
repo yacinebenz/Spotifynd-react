f1a0137875345439fc1d3588cf16223e53c8dfcb
yacine <yacinebenz3@gmail.com>
2017-04-13 19:14:46 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+class App extends Component {
+  render(){
+    return(
+      <div>
+        Spotyfind in app
+      </div>
+    )
+  }
+}
+export default App;
