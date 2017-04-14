eaa3f234cd5a743f37aa77d31e63dca983965c44
yacine <yacinebenz3@gmail.com>
2017-04-14 17:41:32 +0100

    added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
+import './App.css';
+import {FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
 class App extends Component {
   render(){
     return(
-      <div>
+      <div className="App">
         <div className="App-title">Spotyfind</div>
-        <div>
-          <input placeholder="search an artist"/>
-          <button>button</button>
-        </div>
+        <FormGroup>
+        <InputGroup>
+        <FormControl
+          type="text"
+          placeholder="Search for an Artist"
+          />
+        <InputGroup.Addon>
+          <Glyphicon glyph="search"></Glyphicon>
+        </InputGroup.Addon>
+        </InputGroup>
+        </FormGroup>
+
         <div className="Profile">
           <div>Artist Picture</div>
           <div>Artist Name</div>
